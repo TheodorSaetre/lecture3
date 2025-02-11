a7bd8c7ecc4758a6c1ced22a48b481be21db9277
TheodorSaetre <thst035@student.kristiania.no>
2025-02-11 16:39:56 +0100

    la til fylker og skoler

--- src/src/modules/app/application.tsx
+++ src/src/modules/app/application.tsx
@@ -6,19 +6,57 @@ import { useGeographic } from "ol/proj";
 
 // Styling of OpenLayers components like zoom and pan controls
 import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
+import { Circle, Stroke, Style, Text } from "ol/style";
 
 // By calling the "useGeographic" function in OpenLayers, we tell that we want coordinates to be in degrees
 //  instead of meters, which is the default. Without this `center: [10.6, 59.9]` brings us to "null island"
 useGeographic();
 
+let municipalityLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/lecture3/geojson/fylker.json",
+    format: new GeoJSON(),
+  }),
+  style: new Style({
+    stroke: new Stroke({
+      color: "blue",
+      width: 1,
+    }),
+  }),
+});
+
+let schoolLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/lecture3/geojson/skoler.json",
+    format: new GeoJSON(),
+  }),
+  style: (f) =>
+    new Style({
+      image: new Circle({
+        radius: 20,
+        stroke: new Stroke({
+          color: "blue",
+          width: 2,
+        }),
+      }),
+      text: new Text({ text: f.getProperties().navn }),
+    }),
+});
 // Here we create a Map object. Make sure you `import { Map } from "ol"`. Otherwise, the standard Javascript
 //  map data structure will be used
 const map = new Map({
   // The map will be centered on a position in longitude (x-coordinate, east) and latitude (y-coordinate, north),
   //   with a certain zoom level
-  view: new View({ center: [10.7917, 59.9005], zoom: 18 }),
+  view: new View({ center: [10.7917, 59.9005], zoom: 9 }),
   // map tile images will be from the Open Street Map (OSM) tile layer
-  layers: [new TileLayer({ source: new OSM() })],
+  layers: [
+    new TileLayer({ source: new OSM() }),
+    municipalityLayer,
+    schoolLayer,
+  ],
 });
 
 // A functional React component
